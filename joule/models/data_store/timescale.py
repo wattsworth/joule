aa772801286a13b1ca9f63496ee793849b2b96d3
John Donnal <jdonnal@Johns-MBP.office.wattsworth.net>
2018-10-26 16:06:57 -0400

    configuration for python setup.py tests to run nose2

---
+++ joule/models/data_store/timescale.py
@@ -0,0 +1,54 @@
+import asyncio
+from typing import List, Optional, Callable, Coroutine, Dict
+import numpy as np
+
+from joule.models import Stream, pipes
+
+
+from joule.models.data_store.data_store import DataStore, StreamInfo, DbInfo
+
+Loop = asyncio.AbstractEventLoop
+
+class TimescaleStore(DataStore):
+
+    def __init__(self, server: str, insert_period: float,
+                 cleanup_period: float, loop: Loop):
+        self.server = server
+        self.decimation_factor = 4
+        self.insert_period = insert_period
+        self.cleanup_period = cleanup_period
+        self.loop = loop
+
+    async def initialize(self, stream: List[Stream]) -> None:
+        pass
+
+    async def insert(self, stream: 'Stream',
+                     data: np.ndarray, start: int, end: int):
+        pass
+
+    async def spawn_inserter(self, stream: 'Stream', pipe: pipes.Pipe,
+                             loop: Loop, insert_period=None) -> asyncio.Task:
+        pass
+
+    async def intervals(self, stream: 'Stream', start: Optional[int], end: Optional[int]):
+        pass
+
+    async def extract(self, stream: 'Stream', start: Optional[int], end: Optional[int],
+                      callback: Callable[[np.ndarray, str, bool], Coroutine],
+                      max_rows: int = None, decimation_level=None):
+        pass
+
+    async def remove(self, stream: 'Stream', start: Optional[int], end: Optional[int]):
+        pass
+
+    async def destroy(self, stream: 'Stream'):
+        pass
+
+    async def info(self, streams: List['Stream']) -> Dict[int, StreamInfo]:
+        pass
+
+    async def dbinfo(self) -> DbInfo:
+        pass
+
+    def close(self):
+        pass
