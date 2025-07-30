a95b86223d603fefd205aa514b5f3865ed248c73
John S Donnal <donnal@usna.edu>
2025-07-30 16:19:33 -0400

    removed docserver from repository, not used

--- docserver/src/app/safe-pipe.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SafePipe } from './safe-pipe.pipe';
-
-describe('SafePipePipe', () => {
-  it('create an instance', () => {
-    const pipe = new SafePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
